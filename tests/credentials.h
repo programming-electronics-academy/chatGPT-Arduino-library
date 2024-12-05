d5ba2679d8ccfde9c913ccbac98ea671837fc088
ProgrammingElectronicsAcademy <michael@programmingelectronics.com>
2024-12-05 13:49:13 -0500

    refactor: remove false positive secret

--- tests/credentials.h
+++ tests/credentials.h
@@ -1,9 +1,4 @@
 #ifndef Credentials_h
 #define Credentials_h
 
-/* Keep all these private */
-const char* ssid = "UnderGroundLayer";  // Change to your WiFi Network name
-const char* password = "MWaHaHaHa7#!";  // Change to your WiFi password
-const char* key = "";                   // Your private API Key see https://bit.ly/OpenAI-Dev to get setup
-
 #endif
